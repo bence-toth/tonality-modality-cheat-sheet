4537ac7dffe325e210e73d94da5bdc4b64f63e1a
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-09-13 23:22:26 +0200

    Updates

---
+++ precache-manifest.55e79071a955d4d77a3023a8cee12a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed25957f5b8faa035e71a91c3f4dec9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca2de6ba2da8da22ceb",
+    "url": "/static/css/main.e1494839.chunk.css"
+  },
+  {
+    "revision": "26ee43523d1fbca9ae8b",
+    "url": "/static/js/2.446f6985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.446f6985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca2de6ba2da8da22ceb",
+    "url": "/static/js/main.d8252c61.chunk.js"
+  },
+  {
+    "revision": "0163cd41f1df62fa78cc",
+    "url": "/static/js/runtime-main.b6f2938c.js"
+  }
+]);
